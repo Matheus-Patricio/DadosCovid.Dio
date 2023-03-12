b5c0d3d6efe148bfbee438dc38d492eb778d0f5c
paidepet <matheustwirl@gmail.com>
2023-03-12 17:23:05 -0300

    DadosCovid19

---
+++ DadosCovid.py
@@ -0,0 +1,36 @@
+
+
+import pandas as pd
+import plotly.express as px
+import streamlit as st 
+
+#streamlit run codigoBase.py
+
+#LENDO O DATASET
+df = pd.read_csv('https://raw.githubusercontent.com/wcota/covid19br/master/cases-brazil-states.csv')
+
+#MELHORANDO O NOME DAS COLUNAS DA TABELA
+df = df.rename(columns={'newDeaths': 'Novos óbitos','newCases': 'Novos casos','deaths_per_100k_inhabitants': 'Óbitos por 100 mil habitantes','totalCases_per_100k_inhabitants':'Casos por 100 mil habitantes', 'vaccinated': 'Vacinados', 'vaccinated_per_100_inhabitants': 'Vacinados a cada 100 mil habitantes', 'vaccinated_single': 'Vacinados - Primeira Dose', 'vaccinated_single_per_100_inhabitants' : 'Apenas primeira dose por 100 mil habitantes', 'vaccinated_second':'Vacinados - Segunda Dose', 'vaccinated_second_per_100_inhabitants': 'Vacinados - Segunda Dose por 100 mil hanitantes', 'vaccinated_third':'Vacinados - Terceira Dose', 'vaccinated_third_per_100_inhabitants': 'Vacinados - Terceira Dose por 100 mil habitantes', 'recovered': 'Recuperados'}) 
+
+#SELECÃO DO ESTADO
+estados = list(df['state'].unique())
+state = st.sidebar.selectbox('Qual estado?', estados)
+
+#SELEÇÃO DA COLUNA
+#column ='Casos por 100 mil habitantes'
+colunas = ['Novos óbitos','Novos casos','Óbitos por 100 mil habitantes','Casos por 100 mil habitantes','Vacinados', 'Vacinados a cada 100 mil habitantes','Vacinados - Primeira Dose','Apenas primeira dose por 100 mil habitantes','Vacinados - Segunda Dose', 'Vacinados - Segunda Dose por 100 mil hanitantes', 'Vacinados - Terceira Dose', 'Vacinados - Terceira Dose por 100 mil habitantes', 'Recuperados']
+column = st.sidebar.selectbox('Qual tipo de informação?', colunas)
+
+#SELEÇÃO DAS LINHAS QUE PERTECEM AO ESTADO 
+df = df[df['state'] == state]
+
+fig = px.line(df, x="date", y=column, title=column + ' - ' + state)
+fig.update_layout( xaxis_title='Data', yaxis_title=column.upper(), title = {'x':0.5})
+
+st.title('DADOS COVID - BRASIL')
+st.write('Nessa aplicação, o usuário tem a opção de escolher o estado e o tipo de informação para mostrar o gráfico. Utilize o menu lateral para alterar a mostragem.')
+
+st.plotly_chart(fig, use_container_width=True)
+
+st.caption('Os dados foram obtidos a partir do site: https://github.com/wcota/covid19br')
+
